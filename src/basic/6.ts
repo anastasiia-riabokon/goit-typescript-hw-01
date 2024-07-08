7f9fd7e5a80e8dfe1f9ebe59c5fa8b892b4a0c1f
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-07-08 14:29:20 +0300

    feat(app): task 6

---
+++ src/basic/6.ts
@@ -0,0 +1,48 @@
+//? Створіть інтерфейс User для типізації об'єктів, які містять такі властивості. Зверніть увагу, що адреса є необов'язковою властивістю.
+//todo=====JS
+/*
+const mango = {
+    name: 'Mango',
+    age: 30,
+    email: 'john@example.com',
+    address: {
+        city: 'New York',
+        country: 'USA'
+    }
+};
+
+const poly = {
+    name: 'Mango',
+    age: 30,
+    email: 'john@example.com'
+};
+
+*/
+
+//todo=====TS
+
+interface User {
+  name: string;
+  age: number;
+  email: string;
+  address?: {
+    city: string;
+    country: string;
+  };
+}
+
+const mango: User = {
+  name: "Mango",
+  age: 30,
+  email: "john@example.com",
+  address: {
+    city: "New York",
+    country: "USA",
+  },
+};
+
+const poly: User = {
+  name: "Mango",
+  age: 30,
+  email: "john@example.com",
+};
