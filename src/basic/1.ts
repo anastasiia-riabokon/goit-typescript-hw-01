99cb72c364ed21b4edadc677c1d3bc0f47c967ed
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-07-08 14:02:25 +0300

    feat(app): task 1

---
+++ src/basic/1.ts
@@ -0,0 +1,17 @@
+//todo=====JS
+// const age = 50;
+// const username = "Max";
+// const toggle = true;
+// const empty = null;
+// const callback = (a) => {
+//   return 100 + a;
+// };
+
+//todo=====TS
+const age: number = 50;
+const username: string = "Max";
+const toggle: boolean = true;
+const empty: null = null;
+const callback = (a: number): number => {
+  return 100 + a;
+};
