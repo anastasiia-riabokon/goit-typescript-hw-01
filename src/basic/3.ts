a717897cdea2f04dcbdb2349eff0a901858a9121
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-07-08 14:11:18 +0300

    feat(app): task 3

---
+++ src/basic/3.ts
@@ -0,0 +1,6 @@
+//Створіть змінну, яка може містити або рядок, або число (union type)
+let variable: string | number;
+
+//оголосіть змінну, яка може містити лише одне з двох можливих рядкових значень:
+//'enable' або 'disable'(literal type).
+type State = "enable" | "disable";
